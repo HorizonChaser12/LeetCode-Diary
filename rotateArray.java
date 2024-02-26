9ad24950b78e14c867a653307547820e99b1a7d7
HorizonChaser12 <suryakant.mangaraj@gmail.com>
2024-02-27 00:15:53 +0530

    Jan 27

--- rotateArray.java
+++ rotateArray.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 //https://leetcode.com/problems/rotate-array/description/
+//Solutions are full of these same codes and rather than giving intuition behind it they just copy paste, i added the answer so that when i am able to find the different answer , I can compare it . Basically i think this can be done using two pointers too.
 public class rotateArray {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6,7};
@@ -14,12 +15,12 @@ public class rotateArray {
     reversepart(nums,0,k-1);
     reversepart(nums,k,n-1);
 }
-    static void reversepart(int[] nums,int s ,int e){
+    static void reversepart(int[] nums,int s ,int e) {
         int temp;
-        while(s<e){
-            temp=nums[s];
-            nums[s]=nums[e];
-            nums[e]=temp;
+        while (s < e) {
+            temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
             s++;
             e--;
         }
