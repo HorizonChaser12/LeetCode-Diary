cb695aa481d24dce43f0a561ff5e8aa012430848
HorizonChaser12 <suryakant.mangaraj@gmail.com>
2026-02-21 15:08:42 +0530

    Trial Commit

--- constructMinBitwiseArray1.java
+++ constructMinBitwiseArray1.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
 
 public class constructMinBitwiseArray1 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(hasAlternatingBits(n));
+    }
+    static boolean hasAlternatingBits(int n) {
+        StringBuilder str = new StringBuilder();
+        while (n > 0) {
+            str.append(String.valueOf(n % 2));
+            n = n / 2;
+        }
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
